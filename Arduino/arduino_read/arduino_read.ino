20dfedde6466c86fd7fab4d61746952391850c78
minfo10 <118392454+minfo10@users.noreply.github.com>
2024-11-05 14:04:53 +0100

    write/read fixed v1

--- Arduino/arduino_read/arduino_read.ino
+++ Arduino/arduino_read/arduino_read.ino
@@ -3,54 +3,63 @@
 // DDR = INPUT/OUTPUT, PORT = HIGH/LOW, PIN = Lecture
 // https://docs.arduino.cc/hacking/software/PortManipulation
 
-#define pON(port, pin) (port |= bit(pin)) // port OR bit(pin) -> changement que sur pin souhaitÃ©.
+#define pON(port, pin) (port |= bit(pin)) // port OR bit(pin) -> changement que sur pin souhaité.
 #define pOFF(port, pin) (port &= ~ bit(pin)) // port AND NOT bit(pin)
 
-// digitalWrite Tmin > 4us
+const int CLOCK = 7; // Définit la broche pour l'horloge
+const int SC_OUT = 9; // Définit la broche de sortie de signal de contrôle
 
-const int CLOCK = 7;
-const int SC_OUT = 9; // 0100 0000 -> 2d
-
-const int SC_OUTport = 2;
+// Vérifiez que cette valeur correspond au bit correct de la broche SC_OUT sur PORTB
+const int SC_OUTport = (1 << 1); // 0000 0010 pour le bit 1 de PORTB, selon le câblage physique
 
 int checkDebug = 1;
-
 int sRead = 0;
 
-void clk(){
-	pON(PORTD, CLOCK);
-	delayMicroseconds(2);
-	pOFF(PORTD, CLOCK);
-	delayMicroseconds(2);
+void clk() {
+  // Génère une impulsion d'horloge
+  pON(PORTD, CLOCK);
+  delayMicroseconds(2); // Durée de l'impulsion
+  pOFF(PORTD, CLOCK);
+  delayMicroseconds(2);
 }
 
-void setup(){
-	pinMode(CLOCK, OUTPUT);
-	pinMode(SC_OUT, INPUT);
-	Serial.begin(9600);
+void setup() {
+  pinMode(CLOCK, OUTPUT); // Définit l'horloge comme sortie
+  pinMode(SC_OUT, INPUT); // Définit la broche SC_OUT comme entrée
+  Serial.begin(9600); // Initialise la communication série
 }
 
 void loop() {
-	if(checkDebug == 1){
-		sRead = 0;
-		while(Serial.available() == 0){
-			// Mode débuggage.
-		}
-		sRead = Serial.read();
-		checkDebug = 0;
-	} else {
-		if (sRead == 1) {
-			clk();
-			clk();
-			clk();
-			checkDebug = 0; // Rentre directement dans le switch
-			sRead = 2;}
-		else if(sRead == 2){
-			int lect = PORTB;
-				if((lect & SC_OUTport) == SC_OUTport){
-					Serial.write("Wooooo");
-					checkDebug = 1;
-			}
-		}
-		}
-	}
+  if (checkDebug == 1) {
+    // Mode débugage, attend l'entrée de l'utilisateur via le port série
+    sRead = 0;
+    while (Serial.available() == 0) {
+      // Attend une commande utilisateur
+    }
+    sRead = Serial.read() - '0'; // Conversion du caractère en nombre entier
+
+    Serial.print("Received: "); 
+    Serial.println(sRead); // Affiche la valeur reçue pour débogage
+    
+    checkDebug = 0;
+  } else {
+    if (sRead == 1) {
+      Serial.println("ok"); // Affiche "ok" lorsque '1' est reçu
+      // Trois impulsions d'horloge pour l'initialisation
+      clk();
+      clk();
+      clk();
+      checkDebug = 0;
+      sRead = 2;
+    } else if (sRead == 2) {
+      // Lit la valeur de PORTB
+      int lect = PINB; // Correction: utilisation correcte de PINB pour lire le port
+
+      // Vérifie si le bit SC_OUTport est élevé
+      if ((lect & SC_OUTport) == SC_OUTport) {
+        Serial.println("Wooooo");
+        checkDebug = 1;
+      }
+    }
+  }
+}
--- Arduino/arduino_write/arduino_write.ino
+++ Arduino/arduino_write/arduino_write.ino
@@ -4,37 +4,42 @@
 // https://docs.arduino.cc/hacking/software/PortManipulation
 
 // OBJECTIF : Répondre un front montant après 3 impulsions de clk reçues.
-#define pON(port, pin) (port |= bit(pin)) // port OR bit(pin) -> changement que sur pin souhaitÃ©.
+#define pON(port, pin) (port |= bit(pin)) // port OR bit(pin) -> changement que sur pin souhaité.
 #define pOFF(port, pin) (port &= ~ bit(pin)) // port AND NOT bit(pin)
 
-const int IN = 3;
-const int OUT = 4;
-const int INport = 8; // 0001 0000
+const int IN = 3; // Définit la broche pour l'entrée de l'impulsion
+const int OUT = 4; // Définit la broche pour la sortie de l'impulsion
+
+// Correction: vérifiez que cette valeur correspond au bit correct de la broche IN sur PORTD
+const int INport = (1 << IN); // 0000 1000 pour le bit correspondant à la broche 3 sur PORTD
 
 int cpt = 0;
 int check = 0; // Retour à 0 après 3 fronts montants.
 
-void setup(){
-	pinMode(IN, INPUT);
-	pinMode(OUT, OUTPUT);
+void setup() {
+  pinMode(IN, INPUT); // Définit la broche IN comme entrée
+  pinMode(OUT, OUTPUT); // Définit la broche OUT comme sortie
 }
 
-void loop(){
-	int lect = PORTD;
-	if ((lect & INport) == 8){
-		if(check == 0){
-			cpt += 1;
-			check = 1;
-			if(cpt == 3){
-				pON(PORTD, OUT);
-				delay(3);
-				pOFF(PORTD, OUT);
-				cpt = 0;
-			}
-		}
-		
-	} else {
-		check = 0;
-	}
+void loop() {
+  // Lecture de l'état de PORTD
+  int lect = PIND; // Correction: utilisation de PIND pour lire l'état actuel du port
 
+  // Vérifie si la broche IN est élevée (front montant)
+  if ((lect & INport) == INport) {
+    if (check == 0) {
+      cpt += 1; // Incrémente le compteur sur un front montant détecté
+      check = 1;
+      if (cpt == 3) {
+        // Après 3 fronts montants, active la broche OUT
+        pON(PORTD, OUT);
+        delay(3); // Délai de 3 ms (ajustable selon les besoins)
+        pOFF(PORTD, OUT);
+        cpt = 0; // Réinitialise le compteur
+      }
+    }
+  } else {
+    // Réinitialise l'indicateur pour détecter le prochain front montant
+    check = 0;
+  }
 }
